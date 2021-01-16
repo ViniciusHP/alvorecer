6f6fa5d74812e3355ede53aee3f6f678afdd5f27
ViniciusHP <viniciushp97@gmail.com>
2021-01-16 16:00:43 -0300

    Adicionada classe para controlar o slide

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import MenuMobile from './modules/menu-mobile.js';
 import DeslocamentoAteOAlvo from './modules/anima-deslocamento-ate-alvo.js';
 import AcionadorDeClasseAoDeslocamentoScroll from './modules/acionador-classe-deslocamento-scroll.js';
+import Slide from './modules/slide.js';
 
 const menuMobile = new MenuMobile('[data-menu-mobile-btn]', '[data-menu-mobile]');
 menuMobile.init();
@@ -11,3 +12,7 @@ deslocamentoAteOAlvo.init();
 const acionadorDeClasseAoDeslocamentoScroll = 
         new AcionadorDeClasseAoDeslocamentoScroll('[data-acionar-ao-scroll-acionador]', '[data-acionar-ao-scroll-alvo]');
 acionadorDeClasseAoDeslocamentoScroll.init();
+
+const slide = new Slide('[data-slide]', '[data-slide-item]',
+        '[data-slide-controls]', '[data-slide-controls-botao]', 5000);
+slide.init();
