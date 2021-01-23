de98f994270cd3384e8c1cf15261a4f815b1d367
ViniciusHP <viniciushp97@gmail.com>
2021-01-23 16:10:17 -0300

    Adicionado animação para a contagem de número de visitantes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import Slide from './modules/slide.js';
 import TabNav from './modules/tab-nav.js';
 import fetchNumeroVisitantes from './modules/fetch-numero-visitantes.js';
 import Accordion from './modules/accordion.js';
+import AnimaNumero from './modules/anima-numeros.js';
 
 const menuMobile = new MenuMobile('[data-menu-mobile-btn]', '[data-menu-mobile]');
 menuMobile.init();
@@ -28,3 +29,6 @@ fetchNumeroVisitantes('../numero-visitantes.json', '[data-numero-visitantes]');
 const accordion = new Accordion('[data-accordion-item]');
 accordion.init();
 accordion.addCallbackAccordionToggle(() => acionadorDeClasseAoDeslocamentoScroll.recarregaDadosDePosicionamento());
+
+const animaNumeroVisitantes = new AnimaNumero('#opinioes-section', 'ativo', '[data-anima-numeros]', 2000);
+animaNumeroVisitantes.init();
