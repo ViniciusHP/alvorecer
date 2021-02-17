1631a0b2311fc11db51d9784ce64214317689488
ViniciusHP <viniciushp97@gmail.com>
2021-02-17 16:40:49 -0300

    Adicionado polyfill para o window.fetch

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['@babel/polyfill', './js/script.js'],
+  entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
   output: {
     path: path.resolve(__dirname, './'),
     filename: 'main.js',
