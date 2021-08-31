ea0cbe0939461cd3a70ccccbf1782e33b0def6d5
ViniciusHP <viniciushp97@gmail.com>
2021-08-30 21:20:22 -0300

    Adicionado comentários em Accordion

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,4 +1,11 @@
+/**
+ * Classe responsável pela funcionalidade de Accordion.
+ */
 export default class Accordion {
+  /**
+   * @param {*} elementosAcionadores Seletor CSS dos elementos acionadores.
+   * @param {*} classeAtiva Classe CSS que será adicionada ao elemento acinado.
+   */
   constructor(elementosAcionadores, classeAtiva) {
     this.elementosAcionadores = document.querySelectorAll(elementosAcionadores);
 
@@ -13,6 +20,10 @@ export default class Accordion {
     this.acionadorHandler = this.acionadorHandler.bind(this);
   }
 
+  /**
+   * Inicializa a funcionalidade de Accordion.
+   * @returns Instância atual de Accordion.
+   */
   init() {
     if (this.elementosAcionadores.length) {
       this.addEventAccordion();
@@ -21,12 +32,19 @@ export default class Accordion {
     return this;
   }
 
+  /**
+   * Adiciona evento de click aos acionadores.
+   */
   addEventAccordion() {
     this.elementosAcionadores.forEach((acionador) => {
       acionador.addEventListener('click', this.acionadorHandler);
     });
   }
 
+  /**
+   * Método responsável por tratar o evento de click do acionador.
+   * @param {*} event - evento de click
+   */
   acionadorHandler(event) {
     event.preventDefault();
     const acionador = event.currentTarget;
@@ -43,10 +61,19 @@ export default class Accordion {
     }
   }
 
+  /**
+   * Adiciona uma callback que será chamada toda vez que o Accordion
+   * alternar entre aberto e fechado.
+   * @param {*} callback - função que será chamada
+   */
   addCallbackAccordionToggle(callback) {
     this.callbackAccordionToggle.push(callback);
   }
 
+  /**
+   * Executa o código de todas as callbacks que foram adicionadas através do método
+   * addCallbackAccordionToggle.
+   */
   executeAllCallbacksAccordionToggle() {
     this.callbackAccordionToggle.forEach((callback) => {
       callback();
