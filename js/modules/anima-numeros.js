ff5e2dc9d6fcc244dfabd15a3ea68d43e1498102
ViniciusHP <viniciushp97@gmail.com>
2021-08-31 21:35:48 -0300

    Comentada classe AnimaNumero

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,4 +1,14 @@
+/**
+ * Classe que executa a animação de contagem progressiva até determinado número.
+ */
 export default class AnimaNumero {
+  /**
+   * @param {*} seletorElementoASerObservado Seletor CSS do elemento que terá classe
+   * a ser observada.
+   * @param {*} classeASerObservada Classe CSS que será observada.
+   * @param {*} seletorElementoParaAnimar Seletor CSS do elemento que contém o número.
+   * @param {*} milisegundosAnimacao Tempo de duração da animação.
+   */
   constructor(seletorElementoASerObservado, classeASerObservada,
     seletorElementoParaAnimar, milisegundosAnimacao) {
     this.elementoASerObservado = document.querySelector(seletorElementoASerObservado);
@@ -9,6 +19,10 @@ export default class AnimaNumero {
     this.observerCallback = this.observerCallback.bind(this);
   }
 
+  /**
+   * Inicializa as funcionalidades.
+   * @returns Instância atual.
+   */
   init() {
     if (this.elementoASerObservado && this.classeASerObservada && this.elementoParaAnimar) {
       this.addObserver();
@@ -19,11 +33,19 @@ export default class AnimaNumero {
     return this;
   }
 
+  /**
+   * Método que adiciona um observador a mutação de atributos de 'elementoASerObservado'
+   */
   addObserver() {
     this.observer = new MutationObserver(this.observerCallback);
     this.observer.observe(this.elementoASerObservado, { attributes: true });
   }
 
+  /**
+   * Método que processa as mutações de atributos.
+   * Se a 'classeASerObservada' estiver presente em 'elementoASerObservado',
+   * a animação dos números será iniciada.
+   */
   observerCallback() {
     if (this.elementoASerObservado.classList.contains(this.classeASerObservada)) {
       this.animarNumero();
@@ -31,6 +53,9 @@ export default class AnimaNumero {
     }
   }
 
+  /**
+   * Método que executa a animação dos números
+   */
   animarNumero() {
     const total = Number(this.elementoParaAnimar.innerText);
     const iteracoes = 100;
